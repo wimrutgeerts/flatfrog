889b1a6de48ad13f33c4dc5e9fcaddb8c69a411d
wimrutgeerts <wim.rutgeerts@gmail.com>
2012-07-12 18:07:03 +0200

    added random backgroundcolor

--- app/controllers/Index.java
+++ app/controllers/Index.java
@@ -1,15 +1,26 @@
 package controllers;
 
 import play.mvc.*;
+import views.html.index;
 
 public class Index {
 
+	private static String [] colors = {"FF7F00","A52A2A", "3366FF", "00CC66"};
+	
+	public static Result index() {
+	  	return Results.ok(index.render("Your new application is ready.", getRandomColor()));
+	}
+	
 	public static Result signIn(){
 		return Results.TODO;
 	}
 	
 	public static Result signUp(){
 		return Results.TODO;
+	}
+	
+	private static String getRandomColor() {
+		return colors[((int)(Math.random()*colors.length))];
 	}
 	
 }
