14658881bae3c69c541581c7884c81de78f740f6
wimrutgeerts <wim.rutgeerts@gmail.com>
2012-07-12 17:47:12 +0200

    started on loginpage

---
+++ app/controllers/Index.java
@@ -0,0 +1,15 @@
+package controllers;
+
+import play.mvc.*;
+
+public class Index {
+
+	public static Result signIn(){
+		return Results.TODO;
+	}
+	
+	public static Result signUp(){
+		return Results.TODO;
+	}
+	
+}
